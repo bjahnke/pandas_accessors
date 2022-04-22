281fa6642bec22593f450073703a202ee9312fe4
bjahnke <bjahnke71@gmail.com>
2022-04-22 12:32:23 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pandas_accessors',
+    version='0.0.0.1',
+    packages=['pda'],
+    url='',
+    license='',
+    author='bjahnke',
+    author_email='bjahnke71@gmail.com',
+    description='framework for using pandas with algorithmic trading',
+    install_requires=[
+        'numpy',
+        'pandas',
+    ]
+)
