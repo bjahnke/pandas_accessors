9fd8c47fa81b9a4baef641c97fabf41f97d50167
bjahnke <bjahnke71@gmail.com>
2022-04-22 12:27:43 -0400

    initial commit

---
+++ pda/accessors.py
@@ -0,0 +1,378 @@
+from __future__ import annotations
+import pandas as pd
+import numpy as np
+from abc import ABCMeta, abstractmethod
+import typing as t
+import pda.utils
+
+
+def _validate(items: t.List[str], mandatory: t.List[str]):
+    col_not_found = [col for col in mandatory if col not in items]
+    if col_not_found:
+        raise AttributeError(f"{col_not_found} expected but not found")
+
+
+class DfAccessorBase(metaclass=ABCMeta):
+    mandatory_cols: t.List[str]
+
+    def __init__(self, df: pd.DataFrame):
+        self._validate(df)
+        self._obj = df
+
+    @classmethod
+    def _validate(cls, df: pd.DataFrame):
+        _validate(df.columns.to_list(), cls.mandatory_cols)
+
+
+class SeriesAccessorBase(metaclass=ABCMeta):
+    mandatory_cols: t.List[str]
+
+    def __init__(self, obj: pd.Series):
+        self._validate(obj)
+        self._obj = obj
+
+    @classmethod
+    def _validate(cls, obj: pd.Series):
+        _validate(obj.index.to_list(), cls.mandatory_cols)
+
+
+def starts(df, col, val):
+    return df[(df[col].shift(1) == val) & (df[col] != val)]
+
+
+def starts_na(df, col) -> pd.Series:
+    return df[pd.isna(df[col].shift(1)) & pd.notna(df[col])]
+
+
+def ends_na(df, col) -> pd.Series:
+    return df[pd.isna(df[col].shift(-1)) & pd.notna(df[col])]
+
+
+def regime_slices(df, regime_col, regime_val=None):
+    assert regime_val in [1, -1, None]
+    rg_starts = starts_na(df, regime_col)
+    rg_ends = ends_na(df, regime_col)
+    res = []
+    for i, end_date in enumerate(rg_ends.index.to_list()):
+        data_slice = df.loc[rg_starts.index[i]: end_date]
+        if regime_val is not None and data_slice[regime_col].iloc[0] == regime_val:
+            res.append(data_slice)
+    return res
+
+
+class PivotRow(SeriesAccessorBase):
+    mandatory_cols = ["start", "end", "rg"]
+
+    def __init__(self, obj: pd.Series):
+        super().__init__(obj)
+
+    def slice(self, dates: pd.Series):
+        """
+        query for dates between the self._obj pivot rows start and end dates
+        TODO, option for inclusive/exclusive
+        :return:
+        """
+        return (self._obj.start < dates) & (dates < self._obj.end)
+
+
+def date_slice(start, end, dates):
+    return (start < dates) & (dates < end)
+
+
+class Table(metaclass=ABCMeta):
+    mandatory_cols: t.List[str]
+
+    def __init__(self, data: pd.DataFrame, name: str):
+        self._validate(data)
+        self._data = data
+        self._name = name
+
+    def _validate(self, df: pd.DataFrame):
+        _validate(df.columns.to_list(), self.__class__.mandatory_cols)
+
+    @property
+    @abstractmethod
+    def mandatory_cols(self) -> t.List[str]:
+        """subclasses must define a class attribute mandatory_cols"""
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def name(self):
+        return self._name
+
+    @classmethod
+    def init_empty_df(cls, index=None) -> pd.DataFrame:
+        df_kwargs = {'columns': cls.mandatory_cols}
+        if index is not None:
+            df_kwargs['index'] = index
+        return pd.DataFrame(**df_kwargs)
+
+    @classmethod
+    def concat(cls, *tables: Table) -> pd.DataFrame:
+        datas = []
+        for table in tables:
+            assert table.__class__ == cls
+            datas.append(table.data)
+        return pd.condat(datas)
+
+
+class PriceTable(Table):
+    mandatory_cols = ["open", "high", "low", "close"]
+
+    def __init__(self, data: pd.DataFrame, name: str):
+        super().__init__(data, name)
+
+    @property
+    def open(self):
+        return self.data.open
+
+    @property
+    def high(self):
+        return self.data.high
+
+    @property
+    def low(self):
+        return self.data.low
+
+    @property
+    def close(self):
+        return self.data.close
+
+    @property
+    def d1_returns(self):
+        return pda.utils.simple_log_returns(self.close)
+
+
+class PivotTable(Table, metaclass=ABCMeta):
+    def __init__(self, data, name, start_date_col, end_date_col):
+        super().__init__(data, name)
+        self._start_col = start_date_col
+        self._end_col = end_date_col
+
+    def unpivot(self):
+        """unpivot the dataframe, filtered by give valid dates"""
+        un_pivoted = unpivot(self.data, self._start_col, self._end_col)
+        un_pivoted.index = un_pivoted.date
+        un_pivoted = un_pivoted.drop('date', axis=1)
+        return un_pivoted
+
+    @property
+    def count(self) -> int:
+        """total row count"""
+        return len(self.data)
+
+    @property
+    def counts(self):
+        """accumulating row count"""
+        return range(1, len(self.data) + 1)
+
+
+class SignalTable(PivotTable):
+    # TODO add current weight column
+    mandatory_cols = [
+        "entry",
+        "trail_stop",
+        "fixed_stop",
+        "dir",
+        "exit_signal_date",
+        "partial_exit_date",
+        "fixed_stop_price"
+    ]
+
+    def __init__(self, data: pd.DataFrame, name="signals"):
+        super().__init__(
+            data=data,
+            name=name,
+            start_date_col="entry",
+            end_date_col="exit_signal_date",
+        )
+
+    @property
+    def entry(self) -> 'pd.Series[pd.Timestamp]':
+        return self.data.entry
+
+    @property
+    def trail_stop(self) -> 'pd.Series[pd.Timestamp]':
+        return self.data.trail_stop
+
+    @property
+    def fixed_stop(self) -> 'pd.Series[pd.Timestamp]':
+        return self.data.fixed_stop
+
+    @property
+    def dir(self) -> 'pd.Series[int]':
+        return self.data.dir
+
+    @property
+    def exit_signal_date(self) -> 'pd.Series[pd.Timestamp]':
+        return self.data.exit_signal_date
+
+    @property
+    def partial_exit_date(self) -> 'pd.Series[pd.Timestamp]':
+        """Note: can also be None"""
+        return self.data.partial_exit_date
+
+    def pyramid_all(self, base_risk) -> pd.Series:
+        """get all risk amortized per regime id"""
+        risk = []
+        for rg_id in self.data.rg_id.unique():
+            signals_by_rg = self.data.loc[self.data.rg_id == rg_id]
+            if signals_by_rg.empty:
+                continue
+            amortized_risk = SignalTable(signals_by_rg).pyramid(base_risk)
+            risk.append(amortized_risk)
+        risk = pd.concat(risk).reset_index(drop=True)
+        return risk
+
+    def pyramid(self, base_risk) -> pd.Series:
+        """Note: should only be used per regime, not on entire signal table"""
+        row_count = pd.Series(data=self.counts) - 1
+        return pda.utils.pyramid(row_count, root=2) * base_risk
+
+    def absolute_stop(self, benchmark_prices: PriceTable):
+        return self.data.fixed_stop_price.values * benchmark_prices.close.loc[self.fixed_stop]
+
+    def entry_prices(self, price_table: PriceTable):
+        return price_table.close.loc[self.entry].reset_index(drop=True)
+
+    def exit_prices(self, price_table: PriceTable):
+        return price_table.close.loc[self.exit_signal_date].reset_index(drop=True)
+
+    def partial_exit_prices(self, price_table: PriceTable):
+        # return price_table.close.loc[self.partial_exit_date.dropna()]
+        data_cpy = self.data.copy()
+        notna = self.data.loc[self.partial_exit_date.notna()].copy()
+        notna['partial_exit_prices'] = price_table.close.loc[notna.partial_exit_date].values
+        data_cpy['partial_exit_prices'] = notna['partial_exit_prices']
+        return data_cpy['partial_exit_prices']
+
+    def static_returns(self, price_table: PriceTable):
+        return (self.exit_prices(price_table) - self.entry_prices(price_table)) * self.dir
+
+    def eqty_risk_shares(self, price_table: PriceTable, eqty, risk, lot=None, fx=None):
+        return pda.utils.eqty_risk_shares(
+            px=self.entry_prices(price_table),
+            sl=self.data.fixed_stop_price,
+            eqty=eqty,
+            risk=risk,
+            lot=lot,
+            fx=fx
+        )
+
+    def get_open_shares(self, price_data):
+        """
+        TODO
+            - add partial_exit_pct to signal df
+            - for each row, get current_open_pct:
+                - current_open_pct = 0 if exit_date < price_data.iloc[-1].index
+                - else: current_open_pct = partial_exit_pct if partial_exit_date != nan
+                - else: current_open_pct = 1.0
+            - current_shares = current_open_pct * shares
+            - return current_shares.cumsum()
+        :param price_data:
+        :return:
+        """
+
+
+class PositionTable(PivotTable):
+    """
+    Table defines pivot periods where
+    """
+
+    mandatory_cols = ["signal_id" "initial_size", "remaining_fraction", "start", "end"]
+
+    def __init__(self, data: pd.DataFrame, name="signals"):
+        super().__init__(
+            data=data,
+            name=name,
+            start_date_col="start",
+            end_date_col="end",
+        )
+
+
+class FrenchStop(Table):
+    mandatory_cols = ['rg_id', 'stop_price']
+
+    def __init__(self, data: pd.DataFrame, name=''):
+        super().__init__(data, name)
+
+    def update(
+        self,
+        pt: pd.DataFrame,
+        st: pd.DataFrame,
+        rg_end: pd.Timestamp,
+    ) -> pd.DataFrame:
+        """
+        set stop loss for prior active entries to cost of previous entry
+        """
+        data_copy = self.data.copy()
+        if st.empty:
+            return data_copy
+
+        # use the last idx up to -2 depending on size of signal table
+        signal = st.loc[
+            st.partial_exit_date.notna() &
+            st.exit_signal_date >= st.iloc[-1].entry
+        ]
+        if signal.empty:
+            return data_copy
+
+        signal = signal.iloc[-min(len(st), 2)]
+        new_french_stop = pt.close.loc[signal.entry]
+        data_copy.loc[st.iloc[-1].partial_exit_date: rg_end, 'stop_price'] = new_french_stop
+        data_copy.loc[st.iloc[-1].partial_exit_date: rg_end, 'rg_id'] = signal.rg_id
+
+        return data_copy
+
+
+class PeakTable(PivotTable):
+    mandatory_cols = [
+        'start',
+        'end',
+        'type',
+        'lvl'
+    ]
+
+    def __init__(self, data: pd.DataFrame, name="peaks"):
+        super().__init__(
+            data=data,
+            name=name,
+            start_date_col="start",
+            end_date_col="end",
+        )
+
+    def start_price(self, price_data):
+        """get price by start date and type, result can be assigned to series"""
+        return self._get_price(price_data, 'start')
+
+    def end_price(self, price_data):
+        """get price by end date and type, result can be assigned to series"""
+        return self._get_price(price_data, 'end')
+
+    def _get_price(self, price_data, date_col):
+        """get price of the given date col based on type (swing hi/swing lo)"""
+        _pt = self.data.copy()
+        return np.where(
+            _pt.type == 1,
+            price_data.close.loc[_pt[date_col]],
+            price_data.close.loc[_pt[date_col]],
+        )
+
+
+def unpivot(
+    pivot_table: pd.DataFrame,
+    start_date_col: str,
+    end_date_col: str,
+    new_date_col="date",
+):
+    """unpivot the given table given start and end dates"""
+    unpivot_table = pivot_table.copy()
+    unpivot_table[new_date_col] = unpivot_table.apply(
+        lambda x: pd.RangeIndex(start=x[start_date_col], stop=x[end_date_col]+1, step=1), axis=1
+    )
+    unpivot_table = unpivot_table.explode(new_date_col, ignore_index=True)
+    # unpivot_table = unpivot_table.drop(columns=[start_date_col, end_date_col])
+    return unpivot_table
---
+++ pda/utils.py
@@ -0,0 +1,363 @@
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class TrailStop:
+    """
+    pos_price_col: price column to base trail stop movement off of
+    neg_price_col: price column to check if stop was crossed
+    cum_extreme: cummin/cummax, name of function to use to calculate trailing stop direction
+    """
+
+    neg_price_col: str
+    pos_price_col: str
+    cum_extreme: str
+    dir: int
+
+    # def init_trail_stop(
+    #     self, price: pd.DataFrame, initial_trail_price, entry_date, rg_end_date
+    # ) -> pd.Series:
+    #     """
+    #     :param rg_end_date:
+    #     :param entry_date:
+    #     :param price:
+    #     :param initial_trail_price:
+    #     :return:
+    #     """
+    #     # trail = price.close.loc[entry_date: rg_end_date] - (initial_trail_price * self.dir)
+    #     # trail = (trail * self.dir).cummax() * self.dir
+    #
+    #     entry_price = price.close.loc[entry_date]
+    #     trail_pct_from_entry = (entry_price - initial_trail_price) / entry_price
+    #     extremes = price.loc[entry_date:rg_end_date, self.pos_price_col]
+    #
+    #     # when short, pct should be negative, pushing modifier above one
+    #     trail_modifier = 1 - trail_pct_from_entry
+    #     # trail stop reaction must be delayed one bar since same bar reaction cannot be determined
+    #     trail_stop: pd.Series = (
+    #         getattr(extremes, self.cum_extreme)() * trail_modifier
+    #     ).shift(1)
+    #     # back patch nan after shifting
+    #     trail_stop.iat[0] = trail_stop.iat[1]
+    #
+    #     return trail_stop
+
+    def init_atr_stop(self, stop_line, entry_date, rg_end_date, modified_atr):
+        _sl = stop_line - (self.dir * modified_atr.loc[entry_date: rg_end_date])
+        return _sl
+
+    def init_trail_stop(self, price, initial_trail_price, entry_date, rg_end_date):
+        init_trail_diff = abs(initial_trail_price - price.close.loc[entry_date])
+        trail = price.close.loc[entry_date: rg_end_date] - (init_trail_diff * self.dir)
+        trail = (trail * self.dir).cummax() * self.dir
+        return trail
+
+    def init_stop_loss(self, price, stop_price, entry_date, rg_end_date):
+        stop_line = pd.Series(index=price.index)
+        stop_line.loc[entry_date: rg_end_date] = stop_price
+        return stop_line
+
+    def exit_signal(self, price: pd.DataFrame, trail_stop: pd.Series) -> pd.Series:
+        """detect where price has crossed price"""
+        return ((trail_stop - price[self.neg_price_col]) * self.dir) >= 0
+
+    def target_exit_signal(self, price: pd.DataFrame, target_price) -> pd.Series:
+        """detect where price has crossed price"""
+        return ((target_price - price['close']) * self.dir) <= 0
+
+    def get_stop_price(
+        self, price: pd.DataFrame, stop_date, offset_pct: float,
+    ) -> float:
+        """calculate stop price given a date and percent to offset the stop point from the peak"""
+        pct_from_peak = 1 - (offset_pct * self.dir)
+        return price['close'].at[stop_date] * pct_from_peak
+
+    def cap_trail_stop(self, trail_data: pd.Series, cap_price, set_price=None) -> pd.Series:
+        """apply cap to trail stop"""
+        if set_price is None:
+            set_price = cap_price
+        trail_data.loc[((trail_data - cap_price) * self.dir) > 0] = set_price
+        return trail_data
+
+
+def pyramid(position, root=2):
+    return 1 / (1 + position) ** (1 / root)
+
+
+def kelly(win_rate, avg_win, avg_loss):
+    """kelly position sizer, returns risk budget as percent"""
+    return win_rate / np.abs(avg_loss) - (1 - win_rate) / avg_win
+
+
+def other_kelly(win_rate, avg_win, avg_loss):
+    return win_rate - (1 - win_rate) / (avg_win/abs(avg_loss))
+
+
+def kelly_fractional():
+    pass
+
+
+def eqty_risk_shares(px, sl, eqty, risk, lot=None, fx=None):
+    r = sl - px
+    budget = eqty * risk
+
+    if fx is not None and fx > 0:
+        budget *= fx
+
+    if lot is None:
+        shares = budget // r
+    else:
+        shares = round(budget // (r * lot) * lot, 0)
+
+    # cap share to less than equity x 2 (includes leverage)
+    nominal_limit = eqty * 2
+    nominal_value = abs(shares * px)
+    exceed_limit = shares.loc[nominal_value > nominal_limit]
+    if not exceed_limit.empty:
+        shares.loc[exceed_limit.index] = (nominal_limit // nominal_value.loc[exceed_limit.index]) * exceed_limit
+
+    return shares
+
+
+def concave(ddr, floor):
+    """
+    For demo purpose only
+    """
+    if floor == 0:
+        concave_res = ddr
+    else:
+        concave_res = ddr**floor
+    return concave_res
+
+
+def convex(ddr, floor):
+    """
+    # obtuse
+    obtuse = 1 - acute
+    """
+    if floor == 0:
+        convex_res = ddr
+    else:
+        convex_res = ddr ** (1 / floor)
+    return convex_res
+
+
+def risk_appetite(eqty, tolerance, mn, mx, span, shape) -> pd.Series:
+    """
+    position sizer
+
+    eqty: equity curve series
+    tolerance: tolerance for drawdown (<0)
+    mn: min risk
+    mx: max risk
+    span: exponential moving average to smooth the risk_appetite
+    shape: convex (>45 deg diagonal) = 1, concave (<diagonal) = -1, else: simple risk_appetite
+    """
+    # drawdown rebased
+    eqty = pd.Series(eqty)
+    watermark = eqty.expanding().max()
+    # all-time-high peak equity
+    drawdown = eqty / watermark - 1
+    # drawdown from peak
+    ddr = 1 - np.minimum(drawdown / tolerance, 1)
+    # drawdown rebased to tolerance from 0 to 1
+    avg_ddr = ddr.ewm(span=span).mean()
+    # span rebased drawdown
+
+    # Shape of the curve
+    if shape == 1:  #
+        _power = mx / mn  # convex
+    elif shape == -1:
+        _power = mn / mx  # concave
+    else:
+        _power = 1  # raw, straight line
+    ddr_power = avg_ddr**_power  # ddr
+
+    # mn + adjusted delta
+    risk = mn + (mx - mn) * ddr_power
+
+    return risk
+
+
+def test_eqty_risk():
+    # px = 2000
+    # sl = 2222
+    px = 2222
+    sl = 2000
+
+    eqty = 100000
+    risk = -0.005
+    fx = 110
+    lot = 100
+
+    res = eqty_risk_shares(px, sl, eqty, risk, fx, lot)
+    print('done')
+
+
+def test_risk_app():
+    equity_curve = 25000
+    tolerance = -0.1
+    min_risk = -0.0025
+    max_risk = -0.0075
+    span = 5
+    shape = 1
+
+    convex_risk = risk_appetite(
+        equity_curve, tolerance, min_risk, max_risk, span, shape
+    )
+    # convex_risk = -convex_risk * peak_equity
+    print('d')
+
+
+def simple_log_returns(prices: pd.Series) -> pd.Series:
+    """calculates log returns of a price series"""
+    return np.log(prices / prices.shift(1))
+
+
+def rolling_grit(cumul_returns, window):
+    rolling_peak = cumul_returns.rolling(window).max()
+    draw_down_squared = (cumul_returns - rolling_peak) ** 2
+    ulcer = draw_down_squared.rolling(window).sum() ** 0.5
+    grit = cumul_returns / ulcer
+    return grit.replace([-np.inf, np.inf], np.NAN)
+
+
+def expanding_grit(cumul_returns):
+    tt_peak = cumul_returns.expanding().max()
+    draw_down_squared = (cumul_returns - tt_peak) ** 2
+    ulcer = draw_down_squared.expanding().sum() ** 0.5
+    grit = cumul_returns / ulcer
+    return grit.replace([-np.inf, np.inf], np.NAN)
+
+
+def rolling_profits(returns, window):
+    profit_roll = returns.copy()
+    profit_roll[profit_roll < 0] = 0
+    profit_roll_sum = profit_roll.rolling(window).sum().fillna(method="ffill")
+    return profit_roll_sum
+
+
+def rolling_losses(returns, window):
+    loss_roll = returns.copy()
+    loss_roll[loss_roll > 0] = 0
+    loss_roll_sum = loss_roll.rolling(window).sum().fillna(method="ffill")
+    return loss_roll_sum
+
+
+def expanding_profits(returns):
+    profit_roll = returns.copy()
+    profit_roll[profit_roll < 0] = 0
+    profit_roll_sum = profit_roll.expanding().sum().fillna(method="ffill")
+    return profit_roll_sum
+
+
+def expanding_losses(returns):
+    loss_roll = returns.copy()
+    loss_roll[loss_roll > 0] = 0
+    loss_roll_sum = loss_roll.expanding().sum().fillna(method="ffill")
+    return loss_roll_sum
+
+
+def profit_ratio(profits, losses):
+    """
+    # if losses goes to zero, ratio is a factor of profits
+    # if profits is zero, ratio should be zero
+    :param profits:
+    :param losses:
+    :return:
+    """
+    _losses = losses.copy()
+    _losses.loc[_losses == 0] = np.nan
+    _losses = abs(_losses.fillna(method="ffill"))
+    pr = profits.fillna(method="ffill") / _losses
+    return pr
+
+
+def rolling_profit_ratio(returns, window):
+    return profit_ratio(
+        profits=rolling_profits(returns, window), losses=rolling_losses(returns, window)
+    )
+
+
+def expanding_profit_ratio(returns):
+    return profit_ratio(
+        profits=expanding_profits(returns), losses=expanding_losses(returns)
+    )
+
+
+def rolling_tail_ratio(cumul_returns, window, percentile, limit):
+    left_tail = np.abs(cumul_returns.rolling(window).quantile(percentile))
+    right_tail = cumul_returns.rolling(window).quantile(1 - percentile)
+    np.seterr(all="ignore")
+    tail = np.maximum(np.minimum(right_tail / left_tail, limit), -limit)
+    return tail
+
+
+def expanding_tail_ratio(cumul_returns, percentile, limit):
+    left_tail = np.abs(cumul_returns.expanding().quantile(percentile))
+    right_tail = cumul_returns.expanding().quantile(1 - percentile)
+    np.seterr(all="ignore")
+    tail = np.maximum(np.minimum(right_tail / left_tail, limit), -limit)
+    return tail
+
+
+def common_sense_ratio(pr, tr):
+    return pr * tr
+
+
+def expectancy(win_rate, avg_win, avg_loss):
+    # win% * avg_win% - loss% * abs(avg_loss%)
+    return win_rate * avg_win + (1 - win_rate) * avg_loss
+
+
+def t_stat(signal_count, trading_edge):
+    sqn = (signal_count**0.5) * trading_edge / trading_edge.std(ddof=0)
+    return sqn
+
+
+def t_stat_expanding(signal_count, edge):
+    """"""
+    sqn = (signal_count**0.5) * edge / edge.expanding().std(ddof=0)
+    return sqn
+
+
+def robustness_score(grit, csr, sqn):
+    """
+    clamp constituents of robustness score to >=0 to avoid positive scores when 2 values are negative
+    exclude zeros when finding start date for rebase to avoid infinite score (divide by zero)
+    """
+    # TODO should it start at 1?
+    _grit = grit.copy()
+    _csr = csr.copy()
+    _sqn = sqn.copy()
+    # the score will be zero if on metric is negative
+    _grit.loc[_grit < 0] = 0
+    _csr.loc[_csr < 0] = 0
+    _sqn.loc[_sqn < 0] = 0
+    exclude_zeros = (_grit != 0) & (_csr != 0) & (_sqn != 0)
+    try:
+        start_date = max(
+            _grit[pd.notnull(_grit) & exclude_zeros].index[0],
+            _csr[pd.notnull(_csr) & exclude_zeros].index[0],
+            _sqn[pd.notnull(_sqn) & exclude_zeros].index[0],
+        )
+    except IndexError:
+        score = pd.Series(data=np.NaN, index=grit.index)
+    else:
+        _grit.loc[_grit.index < start_date] = np.nan
+        _csr.loc[_csr.index < start_date] = np.nan
+        _sqn.loc[_sqn.index < start_date] = np.nan
+        score = (
+            _grit * _csr * _sqn / (_grit[start_date] * _csr[start_date] * _sqn[start_date])
+        )
+    return score
+
+
+def cumulative_returns_pct(returns, min_periods):
+    return returns.expanding(min_periods=min_periods).sum().apply(np.exp) - 1
+
+
+if __name__ == "__main__":
+    test_risk_app()
